fd69250180c1fb1225d7a92e6ae22fe4f950bd52
admirableyetti <ech.emmanuel.ch@gmail.com>
2023-04-08 17:23:55 -0500

    remove unused css

--- src/components/tech.js
+++ src/components/tech.js
@@ -1,25 +1,6 @@
 import { $el } from "../utils";
 import './tech.css'
 
-const css = /*html*/`
-  <style>
-    .techs {
-      margin-top: 10px;
-    }
-    .techs p {
-      color: #007bff;
-      padding: 0px 5px;
-      border: solid 1px #007bff;
-      border-radius: 3px;
-      text-transform: capitalize;
-      display: inline-block;
-      font-size: 14px;
-      margin-bottom: 10px !important;
-      margin-right: 10px !important;
-    }
-  </style>
-`;
-
 export const tech = (text = '') => {
   const html = /*html*/`
     <div class="techs">
--- src/sections/sectionSkills.js
+++ src/sections/sectionSkills.js
@@ -4,45 +4,6 @@ import { textLight } from "../components/text";
 import { title } from "../components/title";
 import './sectionSkills.css'
 
-const css = /*html*/`
-  <style>
-    .skills {
-      display: flex;
-      justify-content: flex-start;
-    }
-    @media screen and (max-width: 768px) {
-      .skills {
-        flex-direction: column;
-      }
-    }
-    .skillsBox {
-      display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
-      row-gap: 20px;
-      column-gap: 50px;
-      width: 100%;
-    }
-    @media screen and (max-width: 768px) {
-      .skillsBox {
-        padding-top: 30px;
-        padding-left: 50px;
-      }
-    }
-    .skillsBox h3 {
-      margin: 0;
-    }
-    .skillsBox ul {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-    }
-    .skillsBox ul li {
-      text-transform: capitalize;
-    }
-  </style>
-`;
-
-
 export const skillSetComponent = ({ skillTitle = '', skills = [] } = {}) => {
   const html = /*html*/`
     <section class="skills">
