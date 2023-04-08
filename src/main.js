f0b26ccffe20ab87cdbe71e23fb58fc748266953
admirableyetti <ech.emmanuel.ch@gmail.com>
2023-04-08 17:42:46 -0500

    add(ui): footer

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { jobs, projects, sideProjects, skills } from './store';
 import { experienceSectionComponent } from './sections/sectionExperience';
 import { projectsSectionComponent } from './sections/sectionProjects';
 import { sideProjectsSectionComponent } from './sections/sectionSideProjects';
+import { footerComponent } from './sections/s';
 
 let ss = 0
 
@@ -19,5 +20,6 @@ const ui = (e) => {
 	main.appendChild(experienceSectionComponent({ sectionTitle: 'Experience'.toUpperCase(), items: jobs }))
 	main.appendChild(projectsSectionComponent({ sectionTitle: 'Projects'.toUpperCase(), items: projects }))
 	main.appendChild(sideProjectsSectionComponent({ sectionTitle: 'Side Projects'.toUpperCase(), items: sideProjects }))
+	main.appendChild(footerComponent)
 }
 ui()
